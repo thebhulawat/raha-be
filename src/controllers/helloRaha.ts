59f629749035469b8483fda99f69354a13bede7e
namsgit <namanbhulawat@gmail.com>
2024-08-05 00:31:37 +0530

    cicd

--- src/controllers/helloRaha.ts
+++ src/controllers/helloRaha.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 import { WithAuthProp } from '@clerk/clerk-sdk-node';
 
 export default function helloRaha(req: Request, res: Response) {
-    const auth = (req as WithAuthProp<Request>).auth;
-    res.json({ message: 'hello Raha', user: auth });
+  const auth = (req as WithAuthProp<Request>).auth;
+  res.json({ message: 'hello Raha' });
 }
